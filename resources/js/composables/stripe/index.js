82bbbf45082e6ab9a534c55093915869dbc1ab5e
Rija Lalaina <rijalaraz@gmail.com>
2022-09-21 12:18:27 +0300

    Correct confirm paymentIntent return url

--- resources/js/composables/stripe/index.js
+++ resources/js/composables/stripe/index.js
@@ -84,7 +84,7 @@ export default function useStripe() {
             elements: elements.value,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: route('order.merci'),
+                return_url: route('checkout.create'),
             },
         });
 
