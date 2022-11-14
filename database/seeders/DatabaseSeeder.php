5e0fd93195e437505e8ec50d1397e9483d2553f6
Rija Lalaina <rijalaraz@gmail.com>
2022-11-14 10:58:34 +0300

    User factory with personal team

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory()
+            ->withPersonalTeam()
+            // ->unverified()
             ->count(10)
             ->has(
                 Order::factory()
